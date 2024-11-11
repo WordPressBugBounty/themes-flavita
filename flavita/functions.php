012731d4dcc7775eac50a0944a8c8889f649773b
Sean Murphy <sean@iamseanmurphy.com>
2024-11-11 12:50:50 +0000

    10.9

--- flavita/functions.php
+++ flavita/functions.php
@@ -2,7 +2,7 @@
 /**
  * Define Theme Version
  */
-define( 'FLAVITA_THEME_VERSION', '10.8' );
+define( 'FLAVITA_THEME_VERSION', '10.9' );
 
 function flavita_css() {
 	$parent_style = 'gradiant-parent-style';
